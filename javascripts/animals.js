8e5faa62c5558c8e1ce8ea603497daa013713761
Debra Gordon <debra.m.gordon@gmail.com>
2016-10-21 13:08:47 -0500

    updating animal prototypal inheritance

--- javascripts/animals.js
+++ javascripts/animals.js
@@ -1,66 +1,90 @@
 var Jungle = (function(jungleClasses){
+//NEED TO CORRECT IIFE SYNTAX!?!
 
-  jungleClasses.Kingdom = {};
-
-  jungleClasses.Kingdom.AnimalClass = function() {
-    this.class = null;
-    this.type = null;
-    this.damage = null;
-    this.health = null;
-
-    this.toString = function() {
-      return this.name;
-    }
-  };
-
-  jungleClasses.Kingdom.Lion = function() {
-    this.class = "Lion";
-    this.type = "4-legged";
+  const Animal = function() {
     this.damage = 50;
-    this.health = 80;
-  };
-  jungleClasses.Kingdom.Lion.prototype = new jungleClasses.Kingdom.AnimalClass();
-
-  jungleClasses.Kingdom.Bear = function() {
-    this.class = "Bear";
-    this.type = "4-legged";
-    this.damage = 60;
-    this.health = 70;
-  };
-  jungleClasses.Kingdom.Bear.prototype = new jungleClasses.Kingdom.AnimalClass();
-
-  jungleClasses.Kingdom.Python = function() {
-    this.class = "Python";
-    this.type = "Ground-hugger";
-    this.damage = 75;
-    this.health = 50;
-  };
-  jungleClasses.Kingdom.Python.prototype = new jungleClasses.Kingdom.AnimalClass();
-
-  jungleClasses.Kingdom.Viper = function() {
-    this.class = "Viper";
-    this.type = "Ground-hugger";
-    this.damage = 80;
-    this.health = 40;
-  };
-  jungleClasses.Kingdom.Viper.prototype = new jungleClasses.Kingdom.AnimalClass();
-
-  jungleClasses.Kingdom.Eagle = function() {
-    this.class = "Eagle";
-    this.type = "2-Winged";
-    this.damage = 40;
-    this.health = 110;
-  };
-  jungleClasses.Kingdom.Eagle.prototype = new jungleClasses.Kingdom.AnimalClass();
-
-  jungleClasses.Kingdom.Hawk = function() {
-    this.class = "Hawk";
-    this.type = "2-Winged";
-    this.damage = 30;
     this.health = 100;
+    this.weapon = null;
   };
-  jungleClasses.Kingdom.Hawk.prototype = new jungleClasses.Kingdom.AnimalClass();
 
-  return jungleClasses;
+//TYPE = four-legged
+
+  const FourLegged = function() {
+    this.movement = "Walker";
+  }
+
+  FourLegged.prototype = new Animal();
+
+//MODELS (2)
+
+  const Lion = function() {
+    this.name = "Lion";
+    this.health += 100;
+    this.damageRange = null;
+  }
+
+  Lion.prototype = new FourLegged();
+
+  const Bear = function() {
+    this.name = "Bear";
+    this.health = null;
+    this.damageRange = null;
+  }
+
+  Bear.prototype = new FourLegged();
+
+//TYPE = Two-winged
+
+  const TwoWinged = function() {
+    this.movement = "Sky-flier";
+  }
+
+  TwoWinged.prototype = new Animal();
+
+//MODELS (2)
+
+  const Eagle = function() {
+    this.name = "Eagle";
+    this.health += 75;
+    this.damageRange = null;
+  }
+
+  Eagle.prototype = new TwoWinged();
+
+  const Hawk = function() {
+    this.name = "Hawk";
+    this.health += 25;
+    this.damageRange = null;
+  }
+
+  Hawk.prototype = new TwoWinged();
+
+//TYPE = scaled
+
+  const ScaleCrawler = function() {
+    this.movement = "Ground-hugger";
+  }
+
+  ScaleCrawler.prototype = new Animal();
+
+//MODELS (2)
+
+  const Viper = function() {
+    this.name = "Viper";
+    this.health += 25;
+    this.damageRange = null;
+  }
+
+  Viper.prototype = new ScaleCrawler();
+
+  const Python = function() {
+    this.name = "Python";
+    this.health += 125;
+    this.damageRange = null;
+  }
+
+  Python.prototype = new ScaleCrawler();
+
+  // return jungleClasses;
 
 })(Jungle || {});
